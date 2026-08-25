2315aa0ecd8cc1bc847110c30c06a9620eba340b
Saniya-uman <saniyabammanalli@gmail.com>
2026-08-25 21:07:14 +0530

    Data Types and variables

---
+++ 01_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let name = "Saniya"
+let age = 20
+let loggedIn = true
+
+
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+//object
+
+console.log(typeof name)
---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 19062005
+let accountEmail = "saniya@gmail.com"
+var accountPassword = "10102004"
+ accountCity = "Jaipur"
+
+console.table([accountId, accountCity, accountEmail, accountPassword])
