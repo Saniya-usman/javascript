ffc4226843afc63919f539d2ba35e19b61cb88b8
Saniya-uman <saniyabammanalli@gmail.com>
2026-08-28 23:48:39 +0530

    Strings

--- 01_dataTypes.js
+++ 01_dataTypes.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { log } = require("node:console");
+
 let name = "Saniya"
 let age = 20
 let loggedIn = true
@@ -36,3 +38,33 @@ const myFunction = function(){
     console.log("Hello World")
 }
 myFunction()
+
+const name1 = "hitesh"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String("saniya")
+
+console.log(gameName.length)
+console.log(gameName.toLowerCase())
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("i"))
+
+const anotheString = gameName.slice(-8, 4)
+console.log(anotheString);
+
+const newStringOne = "          saniya         "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://saniya.com/saniya%20bammanalli"
+
+console.log(url.replace("%20", '-'));
+
+
+
+//================ maths ++======================
+
+console.log(Math);
