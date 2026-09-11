8876067821ce5c122d687ac88d3126f4b248a3b6
Saniya-uman <saniyabammanalli@gmail.com>
2026-09-12 00:01:05 +0530

    revising javascript

---
+++ 01_dates.js
@@ -0,0 +1,14 @@
+
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString());
+console.log(typeof myDate);
+
+
+
+let myCreateDate = new Date(2005, 5, 19)
+console.log(myCreateDate.toDateString());
+
---
+++ arrays.js
@@ -0,0 +1,10 @@
+const myArr = new Array(1, 2, 3, 4, 5)
+console.log(myArr[1]);
+
+myArr.push(6)
+myArr.push(7)
+
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
