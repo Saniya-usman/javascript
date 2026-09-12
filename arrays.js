048f271bd2bf185a3b63c8c7947a77dd5e94d6e3
Saniya-uman <saniyabammanalli@gmail.com>
2026-09-12 23:38:04 +0530

    completed arrays topic

--- arrays.js
+++ arrays.js
@@ -1,10 +1,38 @@
-const myArr = new Array(1, 2, 3, 4, 5)
-console.log(myArr[1]);
+// const myArr = new Array(1, 2, 3, 4, 5)
+// console.log(myArr[1]);
 
-myArr.push(6)
-myArr.push(7)
+// myArr.push(6)
+// myArr.push(7)
 
-console.log(myArr);
+// console.log(myArr);
 
-myArr.pop()
-console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+///
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+//  const heros = marvel_heros.push(dc_heros)
+//  console.log(heros);
+ 
+// const heros1 = marvel_heros.concat(dc_heros)
+// console.log(heros1);
+
+const allHeros = [...marvel_heros, ...dc_heros];
+console.log(allHeros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7,[4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Saniya"));
+
+console.log(Array.from("Saniya"));
+console.log(Array.from({name: "Saniya"}));//intresting 
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1, score2, score3));
