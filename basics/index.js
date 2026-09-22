42baa335fc1624a59179c75be36ce3f517f81439
Saniya-uman <saniyabammanalli@gmail.com>
2026-09-22 23:05:03 +0530

    restarted javascript

---
+++ basics/index.js
@@ -0,0 +1,48 @@
+
+
+console.log("JavaScript is connected!");
+let name = "Saniya"
+
+var n = 20
+var m = 50 
+
+console.log(n+m)
+console.log(n-m);
+console.log(n*m);
+console.log(n/m);
+
+var n = 60
+console.log(n+m);
+console.log("20"+30)
+
+let c = 10
+console.log(typeof(c));
+
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "10");
+console.log(typeof 10);
+
+let a = "10";
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(""));
+console.log(Boolean("hello"));
+console.log(Boolean(null));
+console.log(Boolean([]));
+console.log(Boolean({}));
+
+let obj = {
+    name: "saniya",
+    usn: "3vy23cs039",
+    college:"vtu cpgs klg",
+    branch:"cse",
+    course:"B.tech"
+}
+console.log(obj)
