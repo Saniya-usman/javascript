e059f8d489e1dddc689b5481419c1ac7dfa35fa7
Saniya-uman <saniyabammanalli@gmail.com>
2026-09-23 23:52:30 +0530

    revised objects , arrays, objects in javascript

--- basics/index.js
+++ basics/index.js
@@ -1,48 +1,77 @@
 
 
-console.log("JavaScript is connected!");
-let name = "Saniya"
+// console.log("JavaScript is connected!");
+// let name = "Saniya"
 
-var n = 20
-var m = 50 
+// var n = 20
+// var m = 50 
 
-console.log(n+m)
-console.log(n-m);
-console.log(n*m);
-console.log(n/m);
+// console.log(n+m)
+// console.log(n-m);
+// console.log(n*m);
+// console.log(n/m);
 
-var n = 60
-console.log(n+m);
-console.log("20"+30)
+// var n = 60
+// console.log(n+m);
+// console.log("20"+30)
 
-let c = 10
-console.log(typeof(c));
+// let c = 10
+// console.log(typeof(c));
 
-console.log(typeof null);
-console.log(typeof undefined);
-console.log(typeof "10");
-console.log(typeof 10);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof "10");
+// console.log(typeof 10);
 
-let a = "10";
-let b = 5;
+// let a = "10";
+// let b = 5;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
 
-console.log(Boolean(0));
-console.log(Boolean(1));
-console.log(Boolean(""));
-console.log(Boolean("hello"));
-console.log(Boolean(null));
-console.log(Boolean([]));
-console.log(Boolean({}));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean(""));
+// console.log(Boolean("hello"));
+// console.log(Boolean(null));
+// console.log(Boolean([]));
+// console.log(Boolean({}));
 
-let obj = {
-    name: "saniya",
-    usn: "3vy23cs039",
-    college:"vtu cpgs klg",
-    branch:"cse",
-    course:"B.tech"
-}
-console.log(obj)
+// let obj = {
+//     name: "saniya",
+//     usn: "3vy23cs039",
+//     college:"vtu cpgs klg",
+//     branch:"cse",
+//     course:"B.tech"
+// }
+// console.log(obj)
+
+//This copied the value
+let x = "saniya";
+let y = x;
+
+x = "sanu"
+
+console.log("value of x is:", x);
+console.log("value of y is:",y);
+
+
+//This copied the refrence 
+let p = {name: "saniya"}
+let q = p;
+p.name = "sanu is queen"
+console.log(p);
+console.log(q);
+
+
+//Arrays
+let fruits = ['apple', 'banana', 'mango', 'graps', 'watermillen', 'lichi', 'pinapple']
+
+console.log(fruits);
+console.log(typeof(fruits));
+console.log(fruits[3]);
+
+//functions
+
+
